24fcd1ab2e4d76a4c543789e5dfbae5b3dd7c807
Yanuar Bima <bima.aji1380@gmail.com>
2023-11-25 13:20:24 +0700

    [FIX] minor

--- src/pages/Dashboard/Blog.jsx
+++ src/pages/Dashboard/Blog.jsx
@@ -10,7 +10,7 @@ export default function Blog() {
       <TopArea title="Blog" buttonTitle="Buat Blog" />
       <div className="flex flex-col">
         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div className="inline-block min-w-full sm:px-6 lg:px-8">
             <div className="overflow-hidden">
               <table className="min-w-full text-left text-sm font-light">
                 <thead className="border-b font-medium dark:border-neutral-500">
